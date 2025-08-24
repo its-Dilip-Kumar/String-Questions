3f596b04a5d24020e692c57a37945fd1c7e7c284
Dilip Kumar <dilip.sharma4558@gmail.com>
2025-08-24 20:38:27 +0530

    use stack here

--- MinimumNumberOfBracketReversal.java
+++ MinimumNumberOfBracketReversal.java
@@ -1,8 +1,24 @@
-package string;
 import java.util.*;
 public class MinimumNumberOfBracketReversal {
+    public static int solve(String s){
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(st.isEmpty()){
+                st.push(ch);
+            }else if(!st.isEmpty() && ch=='(' && st.peek()==')'){
+                st.push(ch);
+            }else if(!st.isEmpty() && ch==')' && st.peek()=='('){
+                st.pop();
+            }else{
+                st.push(ch);
+            }
+        }
+        return st.size();
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String str=sc.nextLine();
+        System.out.println(solve(str));
     }
 }
